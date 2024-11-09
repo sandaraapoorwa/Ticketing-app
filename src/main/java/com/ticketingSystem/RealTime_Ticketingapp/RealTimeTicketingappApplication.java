8b7d4247dd3c0ce5eb8b9c3b50504d8b0c1e0113
Sandara  Hettiarachchi <apoorwasandara@gmail.com>
2024-11-09 22:12:25 +0530

    1st commit

---
+++ src/main/java/com/ticketingSystem/RealTime_Ticketingapp/RealTimeTicketingappApplication.java
@@ -0,0 +1,13 @@
+package com.ticketingSystem.RealTime_Ticketingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeTicketingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeTicketingappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ticketingSystem/RealTime_Ticketingapp/RealTimeTicketingappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketingSystem.RealTime_Ticketingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeTicketingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
