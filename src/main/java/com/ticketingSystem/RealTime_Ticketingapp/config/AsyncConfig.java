ad5d37ae7537c9867fd03a5ac279abfaa70b83b8
Sandara  Hettiarachchi <apoorwasandara@gmail.com>
2024-11-10 14:14:44 +0530

    4th commit

---
+++ src/main/java/com/ticketingSystem/RealTime_Ticketingapp/config/AsyncConfig.java
@@ -0,0 +1,28 @@
+package com.ticketingSystem.RealTime_Ticketingapp.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+
+public class AsyncConfig {
+
+    @Bean(name = "taskExecutor")
+
+     public Executor taskExecutor(){
+         ThreadPoolTaskExecutor  executor= new ThreadPoolTaskExecutor();
+         executor.setCorePoolSize(2);
+         executor.setMaxPoolSize(2);
+         executor.setQueueCapacity(100);
+         executor.setThreadNamePrefix("TicketThread-");
+         executor.initialize();
+         return executor;
+
+     }
+}
